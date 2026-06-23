3c38c9de608b90b055feb383a2c2b78614122068
pateladbhut3-byte <pateladbhut3-byte@example.com>
2026-06-23 11:33:40 +0530

    Add seed commands to build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,12 @@ pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+# Seed database
+python manage.py seed_data
+python manage.py seed_members
+python manage.py seed_club_images
+python manage.py seed_club_photos
+python manage.py seed_student_photos
+python manage.py seed_timetable
+
 echo "Build complete"
